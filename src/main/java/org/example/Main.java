0add73fd55427efe3e8bda5a45834ee7278ac4be
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-06-13 12:28:12 +0200

    search books

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
-        System.out.printf("Hello and welcome!");
-
-
+        User user = new User("");
+        Admin admin = new Admin("admin")
+        Scanner scanner = new scanner(System.in);
         }
     }
